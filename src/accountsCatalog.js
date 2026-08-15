cd1c4e0eb50dce1dea0179af784079584d20f955
xatprokuplje <pkcarina@hotmail.com>
2026-08-15 13:28:15 +0200

    Update accountsCatalog.js

--- src/accountsCatalog.js
+++ src/accountsCatalog.js
@@ -11,10 +11,15 @@
 // izostaviš, koristi se username). Posle izmene ovog fajla treba redeploy
 // (kao i za bilo koju izmenu koda) da bi se novi unos pojavio u katalogu.
 export const ACCOUNTS_CATALOG = [
-    { username: "AngelDobric", apikey: "f9c78e399213cc93", nickname: "AngelDobric" },
-    { username: "KikoslavGal", apikey: "17c670f729eabd33", nickname: "zizu" },
-    { username: "Vanesagalaksija", apikey: "eb4f3c42043b873e", nickname: "galaksija" },
+    { username: "AngelDobric", apikey: "5196f8dee8407d95", nickname: "AngelDobric" },
+    { username: "KikoslavGal", apikey: "17c670f729eabd33", nickname: "Kiki" },
+    { username: "Vanesagalaksija", apikey: "eb4f3c42043b873e", nickname: "vanesagal" },
     { username: "RadioXgalaksija", apikey: "3bf3b781c3d015b6", nickname: "zizu" },
+     { username: "Sljivakruska", apikey: "055a11c639778fab", nickname: "sljiva" },
+    { username: " Gladijator5", apikey: "579a900417a85cd2", nickname: "glady" },
+    { username: " RockySlocky", apikey: "f68ad8082f3b3454", nickname: "rocky" },
+    { username: "Krokodil11", apikey: " 0293caa59443d56e", nickname: "kroky" },
+
 
     // Dodaj ispod još naloga po istom obrascu:
     // { username: "NoviNalog", apikey: "NJEGOVAPIKLJUC", nickname: "Nadimak" },
